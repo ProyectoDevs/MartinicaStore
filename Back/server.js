d55218df14e21ef3d9b8b9deb4f9d6efda3bb23f
Mercy <mercyvm03@gmail.com>
2022-10-11 19:53:18 -0500

    Llamado al servidor

--- Back/server.js
+++ Back/server.js
@@ -1,8 +1,8 @@
 const app=require("./app")
  
-//Setear el archivo de configuración
+//Setear O configurar el archivo de configuración
 const dotenv=require("dotenv");
-dotenv.config({path: 'back/config/config.env'})
+dotenv.config({path: 'back/config/config.env'})  //configura el entorno de desarrollo
 
 const server=app.listen(process.env.PORT, () => {
     console.log(`Servidor iniciado en el puerto: ${process.env.PORT} en modo: ${process.env.NODE_ENV}`)
@@ -10,3 +10,4 @@ const server=app.listen(process.env.PORT, () => {
 
     
 
+ 
